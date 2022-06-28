8bb033e6099f52159118085d75534d0c03745872
mrawal <imsr1013@gmail.com>
2022-06-28 15:32:50 +0545

    create base class for inheritance

---
+++ inheritance_oop.py
@@ -0,0 +1,14 @@
+class Animal():
+
+    def __init__(self):
+        print("Animal created")
+
+    def whoAmI(self):
+        print("Animal")
+
+    def eat(self):
+        print('eating')
+
+mya = Animal()
+mya.whoAmI()
+mya.eat()
