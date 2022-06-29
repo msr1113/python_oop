267ca06961db9375dc001d2232adebb555b01808
mrawal <imsr1013@gmail.com>
2022-06-29 10:50:48 +0545

    using for loop print hello 6 times for each item in list

--- python_basic/control_flow.py
+++ python_basic/control_flow.py
@@ -1,7 +1,7 @@
-if 1 == 1:
-    if 1>2:
-        print("hello")
-    elif 3==3:
-        print('equal')
-    else:
-        print('last')
+# for loops
+
+seq = [1,2,3,4,5,6]
+
+for item in seq:
+    print(item)
+    print('print hello 6 times')
