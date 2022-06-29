32b7c99f9adca1415b9935db8c6c24fc250bbfeb
mrawal <imsr1013@gmail.com>
2022-06-29 12:04:09 +0545

    filtering using filter methods using even_bool function and mylist list as parameter

--- python_basic/functions.py
+++ python_basic/functions.py
@@ -3,13 +3,21 @@
 
 # filter
 
+# mylist = [1,2,3,4,5,6,7,8]
+# filtered=[]
+# def even_bool(a):
+#      for num in a:
+#
+#           if num%2 == 0:
+#                filtered.append(num)
+#           print(filtered)
+#
+# print(even_bool(mylist))
+
 mylist = [1,2,3,4,5,6,7,8]
-filtered=[]
-def even_bool(a):
-     for num in a:
 
-          if num%2 == 0:
-               filtered.append(num)
-          print(filtered)
+def even_bool(num):
+     return num%2 == 0
 
-print(even_bool(mylist))
+evens = filter(even_bool,mylist)
+print(list(evens))
