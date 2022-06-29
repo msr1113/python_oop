497dcc9f0af4a40468748f8d0ec82a4f62c1e832
mrawal <imsr1013@gmail.com>
2022-06-29 11:14:19 +0545

    basics of list comprehension by writing calculation first and for loop inside a square bracket

--- python_basic/control_flow.py
+++ python_basic/control_flow.py
@@ -1,4 +1,12 @@
 
-print(list(range(0,5)))
-print(list(range(0,20,2)))
+# list comprehension
+x=[1,2,3,4]
 
+# out = []
+# for num in x:
+#     out.append(num**2)
+#
+# print(out)
+
+out = [num**2 for num in x]
+print(out)
