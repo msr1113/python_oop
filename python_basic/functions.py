50476f0b9644702fbd8d7ea6680123e0fb51f79e
mrawal <imsr1013@gmail.com>
2022-06-29 11:33:16 +0545

    type checking if int or str in being passed to a function

--- python_basic/functions.py
+++ python_basic/functions.py
@@ -1,4 +1,9 @@
-def hello():
-     return("hello")
 
-print(hello())
+def addNum(num1,num2):
+     if type(num1) == type(num2) ==type(10):
+          return num1+num2
+     else:
+          return "Sorry I need integers"
+result = addNum("2",3)
+print(result)
+
